05108caf90c333bb2556b148487a41cb2e3d7385
arcuellar88 <alejandro.rodriguez88@gmail.com>
2014-10-21 18:36:58 +0200

    Cassandra Cluster

--- ulb.ad.cassandra/src/ulb/ad/cassandra/test/TestCassandra.java
+++ ulb.ad.cassandra/src/ulb/ad/cassandra/test/TestCassandra.java
@@ -1,9 +1,16 @@
 package ulb.ad.cassandra.test;
 
+import com.datastax.driver.core.*;
+
 public class TestCassandra {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo GitHub");
+		
+		Cluster cluster;
+		Session session;
+		cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
+		session = cluster.connect("demo");
 	}
 
 }
